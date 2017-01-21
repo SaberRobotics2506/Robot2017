fa1a02405f7fa009e83478bbb6cf7d3f0fb1d23e
Saber Robotics <Saber Robotics>
2017-01-21 09:31:24 -0600

    Initial Commit. Blank Robot

---
+++ src/src/org/usfirst/frc/team2506/robot/Robot.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.team2506.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+
+	@Override
+	public void robotInit() {
+	}
+	
+	@Override
+	public void autonomousInit() {
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+
+	}
+
+	@Override
+	public void teleopPeriodic() {
+	}
+
+	@Override
+	public void testPeriodic() {
+	}
+}
+
