9ba109178c5d10a3d0246b08865b7b10085acc82
GrohallT <Tarynpoke@gmail.com>
2017-03-02 19:24:07 -0600

    Update AutonomousShooting.java

--- src/src/org/usfirst/frc/team2506/robot/AutonomousShooting.java
+++ src/src/org/usfirst/frc/team2506/robot/AutonomousShooting.java
@@ -3,10 +3,15 @@ package org.usfirst.frc.team2506.robot;
 import edu.wpi.first.wpilibj.*;
 
 public class AutonomousShooting {
+	// Tells the robot how far it needs to move back after scoreing the gear to be able to start turning twords the goal.
 	public static final double GEAR_HALFWAY_FEET = 3.5;
+	// Sets the amount of time in ticks that the robot needs to wait after it gets to the gear area so that the pilot can pull up the gear. 
 	public static final double WAIT_TICKS = 150;
+	// Tells the robot how far away from the gear area it needs to be in order to stop moving twords it.
 	public static final double GEAR_RANGE_INCHES = 2.5;
+	// Sets the angle that the robots needs to turn to, this gives it a set point to shoot from every time.
 	public static final double TARGET_ANGLE = 107.5;
+	//  Sets the variable so that every time it is called the robot will move at half speed to prevent it from moving too fast or slow.
 	public static final double ROBOT_SPEED = 0.5;
 	
 	private enum AutonomousStates {
