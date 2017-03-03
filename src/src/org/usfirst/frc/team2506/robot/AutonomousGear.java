0436d36456c5c2e2b0c164b9e537c1fbbb649636
MaxwellIgnatowski <Max53132@gmail.com>
2017-03-02 19:50:34 -0600

    Update AutonomousGear.java

--- src/src/org/usfirst/frc/team2506/robot/AutonomousGear.java
+++ src/src/org/usfirst/frc/team2506/robot/AutonomousGear.java
@@ -4,7 +4,9 @@ import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Ultrasonic;
 
 public class AutonomousGear {
+	// target distance from goal
 	static final double GEAR_RANGE_INCHES = 2.5;
+	// robot motor speed
 	static final double ROBOT_SPEED = 0.5;
 	
 	private enum AutonomousStates {
@@ -16,6 +18,7 @@ public class AutonomousGear {
 	private static SwerveDrive driveTrain;
 	private static Ultrasonic ultrasonic;
 	
+	// method that copies the funcionality of a constructor
 	public static void init(ADXRS450_Gyro gyro, SwerveDrive drive, Ultrasonic ultrasonic) {
 		AutonomousGear.gyro = gyro;
 		driveTrain = drive;
@@ -25,6 +28,8 @@ public class AutonomousGear {
 	public static void run() {
 		switch (autonomousState) {
 			case MovingForward:
+				
+				// drives the robot while it is farther than GEAR_RANGE_INCHES away from the goal, then stops
 				if (ultrasonic.getRangeInches() > GEAR_RANGE_INCHES) {
 					driveTrain.drive(gyro, ROBOT_SPEED, 0, 0);
 				} else {
