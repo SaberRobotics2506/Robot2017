08f53711450156105f1a1a0caabae668a0dbb91d
JoeyConrad <Joseph.Conrad@franklinsabers.org>
2017-03-02 19:50:20 -0600

    Update Intake.java

--- src/src/org/usfirst/frc/team2506/robot/Intake.java
+++ src/src/org/usfirst/frc/team2506/robot/Intake.java
@@ -6,11 +6,10 @@ import edu.wpi.first.wpilibj.*;
 
 public class Intake {
 	private Victor motor;
-	
-	public Intake(int intake) {
+		public Intake(int intake) {
 		this.motor = new Victor(intake);
 	}
-	
+	// Activates the motor to intake
 	public void roll (double speed) {
 		motor.set(speed);
 	}
